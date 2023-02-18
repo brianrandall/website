1260d128b78be0ce02fec15fd4aa6a036b7b75f7
brianrandall <brian19892@gmail.com>
2023-02-18 13:33:24 -0800

    bio update

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -19,7 +19,7 @@ const Home = () => {
     <code>
      <p>Hello! My name is Brian Randall and I am a passionate web developer and software engineer living somewhere in California. When I'm not staring at my computer, you can usually find me on my motorcycle, mountain biking, camping, or at the beach.</p>
 
-    <p>I am constantly pushing myself to learn and grow as a developer and as a human. I thrive on hands on experience and learning new skills, crafts, languages, hobbies, whatever. With a strong background in both front-end dev, back-end dev, design, management, logistics and planning, I have a well-rounded skillset that allows me to tackle any project that comes my way with an "i don't know but i will figure it out" attitude.</p>
+    <p>I am constantly pushing myself to learn and grow as a developer and as a human. I thrive on hands on experience and learning new skills, crafts, languages, hobbies, whatever. With a strong background in both front-end dev, back-end dev, design, management, logistics and planning, I have a well-rounded skillset that allows me to tackle any project that comes my way with an "if i don't know, i will figure it out" attitude.</p>
 
     <p>In addition to my technical skills, I am a highly motivated and detail-oriented individual with a strong work ethic. I pride myself on my ability to collaborate effectively with both technical and non-technical team members, and I am always looking for new ways to improve the user experience.</p>
 
