2abbfbd66592f7f42c84288311ed4ecf8b53eca9
brianrandall <brian19892@gmail.com>
2023-03-29 16:05:10 -0700

    bio update, again

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -20,8 +20,8 @@ const Home = () => {
     <ParticleCloudCanvas />
     <div className='bio'>
       <code>
-        <p>Hello! My name is Brian Randall and I am a passionate web developer and software engineer living somewhere in California. When I'm not staring at my computer, you can usually find me on my motorcycle, mountain biking, camping, or at the beach.</p>
-        <p>I am constantly pushing myself to learn and grow as a developer and as a human. I thrive on hands on experience and learning new skills, crafts, languages, hobbies, whatever. With a strong background in both front-end dev, back-end dev, design, management, logistics and planning, I have a well-rounded skillset that allows me to tackle any project that comes my way.</p>
+        <p>Hello! My name is Brian Randall and I am a web developer and software engineer living somewhere in California. I have a strong background in full-stack development, design, and 10+ years experience in management, logistics & planning. When I'm not at my computer, you can find me on my motorcycle, mountain biking, camping, or at the beach.</p>
+        <p>I am constantly pushing myself to learn and grow as a developer and as a human. I thrive with hands-on-experience and learning new skills, crafts, languages, hobbies, whatever. With a strong background in full-stack development, design, management, logistics and planning, I have a well-rounded skillset that allows me to tackle any project that comes my way.</p>
         <p>In addition to my technical skills, I am a highly motivated and detail-oriented individual with a strong work ethic. I pride myself on my ability to collaborate effectively with both technical and non-technical team members, and I am always looking for new ways to improve the user experience.</p>
         <p>However you found yourself here, find my email on the contact page and say hi!</p>
       </code>
